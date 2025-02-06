707054a5a85413e58be814c8bb608d30d7c915cd
dharnishp <128043459+dharnishp@users.noreply.github.com>
2025-02-06 22:21:29 +0530

    Update and rename Assignment3.java to Example10.java

--- Example10.java
+++ Example10.java
@@ -1,4 +1,4 @@
-package com.dharnish.assignment;
+package com.dharnish.jdbcPrograms;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Assignment3 {
+public class Example10 {
 
 	public static void main(String[] args) throws ClassNotFoundException,SQLException{
 		Class.forName("com.mysql.cj.jdbc.Driver");
