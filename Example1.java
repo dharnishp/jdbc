602ce162ad1fb705afa69d118241228300e028f8
dharnishp <128043459+dharnishp@users.noreply.github.com>
2025-02-09 18:13:35 +0530

    Update Example1.java

--- Example1.java
+++ Example1.java
@@ -1,5 +1,7 @@
+/*
+Inserting records in relation database through the java application.
+*/
 package com.dharnish.jdbc.Programs;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
