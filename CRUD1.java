7c5a0176b925ce324c2bfe43a801d7d421d97a31
dharnishp <128043459+dharnishp@users.noreply.github.com>
2025-02-09 18:12:23 +0530

    Update CRUD1.java

--- CRUD1.java
+++ CRUD1.java
@@ -1,5 +1,7 @@
+/*
+CRUD operations or Batch proceesing in JDBC API.
+*/
 package com.dharnish.batchprocessing;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
